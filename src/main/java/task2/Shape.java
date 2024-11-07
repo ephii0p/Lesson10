f5be4bd94cfb6669b059de01c946f62bd94e02df
ephii0p <belunkind@gmail.com>
2024-11-07 09:27:31 +0400

    Lesson10

--- src/main/java/task2/Shape.java
+++ src/main/java/task2/Shape.java
@@ -1,8 +1,18 @@
 package task2;
 
 public interface Shape {
-    double calculatePerimeter();
-    double calculateArea();
+    default double calculatePerimeter() {
+        return 0;
+    }
+
+    // Дефолтный метод для расчета площади
+    default double calculateArea() {
+        return 0;
+    }
+
+    // Метод для получения цвета заливки
     String getFillColor();
+
+    // Метод для получения цвета границы
     String getBorderColor();
 }
